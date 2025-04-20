2e05dcfa02822deb440805137b714e02d109325b
Ovie Bazunu <ob97@student.le.ac.uk>
2025-04-20 22:50:29 +0100

    Added images and made homepage

--- my-koopa-surprise/src/components/Homepage.jsx
+++ my-koopa-surprise/src/components/Homepage.jsx
@@ -1,44 +1,47 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
+import "../stylesheet/Homepage.css";
 
 const Homepage = () => {
   return (
     <div id="homepage">
-      <div className="w-full h-screen object-cover object-left">
-        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center items-start items-center">
-          <div className="flex space-x-4 mb-6">
+      <div className="homepage-wrapper">
+        <div className="homepage-container">
+          <div className="image-container">
             <img
               src="/images/image1.jpg"
               alt="Image 1"
-              className="w-40 h-40 object-cover rounded-lg shadow-md"
+              className="custom-img"
             />
             <img
-              src="/images/image2.jpg"
-              alt="Image 2"
-              className="w-40 h-40 object-cover rounded-lg shadow-md"
+              src="/images/image13.jpg"
+              alt="Image 13"
+              className="custom-img"
             />
           </div>
-
-          <h1 className="flex sm:text-5xl text-3xl pt-4 sm:pl-0 pl-20 text-gray-800">
-            <TypeAnimation
-              sequence={[
-                "Happy Birthday Satri !!!",
-                2000,
-                "Happy Birthday Koopa !!!",
-                2000,
-                "Happy Birthday Baby !!!",
-                2000,
-              ]}
-              wrapper="span"
-              speed={40}
-              style={{
-                fontSize: "1em",
-                display: "inline-block",
-                paddingLeft: "5px",
-              }}
-              repeat={Infinity}
-            />
-          </h1>
+          <div>
+            <h1>
+              <TypeAnimation
+                sequence={[
+                  "Happy Birthday Satri !!!",
+                  2000,
+                  "Happy Birthday Koopa !!!",
+                  2000,
+                  "Happy Birthday Baby !!!",
+                  2000,
+                ]}
+                wrapper="span"
+                speed={40}
+                style={{
+                  fontSize: "1em",
+                  display: "inline-block",
+                  paddingLeft: "5px",
+                }}
+                repeat={Infinity}
+              />
+            </h1>
+            <h2>I hope you enjoyed it :)</h2>
+          </div>
         </div>
       </div>
     </div>
