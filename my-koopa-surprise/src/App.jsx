6557d1592908a12adf4fdd28b6782ee1b6b7d530
Ovie G Bazunu <47861826+oviebazunu@users.noreply.github.com>
2025-05-15 08:32:37 +0100

    updated name

--- my-koopa-surprise/src/App.jsx
+++ my-koopa-surprise/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Homepage from "./components/Homepage";
 import Sidebar from "./components/Sidebar";
-import Firstdate from "./assets/data/firstdate";
+import Firstdate from "./assets/data/Firstdate";
 import OutLeicester1 from "./assets/data/OutLeicester1";
 import OutManchester1 from "./assets/data/OutManchester1";
 import Halloween from "./assets/data/Halloween";
