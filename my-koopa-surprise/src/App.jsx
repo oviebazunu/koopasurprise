1b786553bc01a22c2e338175d66127fba0bf5ced
Ovie G Bazunu <47861826+oviebazunu@users.noreply.github.com>
2025-05-20 15:33:21 +0100

    updated import

--- my-koopa-surprise/src/App.jsx
+++ my-koopa-surprise/src/App.jsx
@@ -8,7 +8,7 @@ import OutManchester1 from "./assets/data/OutManchester1";
 import Halloween from "./assets/data/Halloween";
 import Netherlands from "./assets/data/Netherlands";
 import Morocco from "./assets/data/Morocco";
-import Bradgate from "./assets/data/bradgate";
+import Bradgate from "./assets/data/Bradgate";
 
 function App() {
   return (
